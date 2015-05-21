e9f3e6f318590caec36ee8677c0498b60ce87257
Mikael Manukyan <arm.localhost@gmail.com>
2015-05-21 19:59:17 +0400

    fix for native Number

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var BigNumber = require('bignumber.js');
 var Decimal = require('decimal.js');
 
+var lib = process.argv[2];
+var isNative = lib !== 'big' && lib !== 'dec';
+
 var const108 = getNumber(108);
 var const815 = getNumber(815);
 var const1500 = getNumber(1500);
@@ -8,15 +11,20 @@ var const4 = getNumber(4);
 var const4d25 = getNumber(17/4); // 4.25
 
 function getNumber(number) {
-	if (process.env.LIB === 'big') {
+	if (lib === 'big') {
 		return new BigNumber(number);
-	} else if (process.env.LIB === 'dec') { 
+	} else if (lib === 'dec') { 
 		return new Decimal(number);
+	} else {
+		return number;
 	}
 }
 
 function f(y, z) {
-	//108 - (815-1500/z)/y
+	if (isNative) {
+		return 108 - (815-1500/z)/y;
+	}
+
 	return const108.minus(const815.minus(const1500.dividedBy(z)).dividedBy(y));
 }
 
